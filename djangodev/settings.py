e070291b29e5965570539ee6991ab290623481e4
wonkybream <sampo.l@protonmail.com>
2021-11-21 18:43:47 +0200

    Start to implement application for running Django tests with Python testcontainers package

--- djangodev/settings.py
+++ djangodev/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rdtwt.apps.RdtwtConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# RDTWT SETTINGS
+RDTWT_POSTGRESQL_IMAGE = 'postgres:14.1'
